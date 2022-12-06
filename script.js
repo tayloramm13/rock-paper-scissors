ad933ea88939662456623a3942d7eb95d6110437
tayloramm13 <t.bird.code13@gmail.com>
2022-12-06 12:28:46 -0700

    Create funciton to establish winner of game

--- script.js
+++ script.js
@@ -21,16 +21,23 @@ selectionButtons.forEach(selectionButton => {
     selectionButton.addEventListener('click', e => {
         const selectionName = selectionButton.dataset.selection
         const selection =  SELECTIONS.find(selection => selection.name === selectionName)
-        makeSelection(selectionName)
+        makeSelection(selection)
     })
 })
 
 function makeSelection(selection) {
     const computerSelection = randomSelection()
+    const yourWinner = isWinner(selection, computerSelection)
+    const computerWinner = isWinner(computerSelection, selection)
     console.log(computerSelection); 
 }
 
+function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name
+}
+
+
 function randomSelection() {
     const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
-    return SELECTIONS(randomIndex)
+    return SELECTIONS[randomIndex]
 }

cf9546122970a3d7aa1f621dece0f0fd6e148174
tayloramm13 <t.bird.code13@gmail.com>
2022-12-06 12:20:58 -0700

    Add random selecton generator for computers choice

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 const selectionButtons = document.querySelectorAll('[data-selection]')
+const SELECTIONS = [
+    {
+        name: 'rock',
+        emoji: '🪨',
+        beats: 'scissors'
+    }, 
+    {
+        name: 'toilet-paper',
+        emoji: '🧻',
+        beats: 'rock'
+    }, 
+    {
+        name: 'scissors',
+        emoji: '✂️',
+        beats: 'toilet-paper'
+    }, 
+]
 
 selectionButtons.forEach(selectionButton => {
     selectionButton.addEventListener('click', e => {
-        const selectionName = selectionButton.dataset.selection 
+        const selectionName = selectionButton.dataset.selection
+        const selection =  SELECTIONS.find(selection => selection.name === selectionName)
         makeSelection(selectionName)
     })
 })
 
 function makeSelection(selection) {
-    console.log(selection); 
+    const computerSelection = randomSelection()
+    console.log(computerSelection); 
+}
+
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS(randomIndex)
 }
